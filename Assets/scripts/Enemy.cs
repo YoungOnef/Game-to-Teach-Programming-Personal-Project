8bacca35a26a4232871b60149c8386d22782f3ff
YoungOne <72264732+YoungOnef@users.noreply.github.com>
2023-01-07 16:39:28 +0000

    Improved Enemy Settings implementation
    Improved Enemy Settings implementation

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,29 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] float _speed = 1.0f;
-    [SerializeField] float _waitTime = 1f;
-    [SerializeField] float minDystans = 0.1f;
-    [SerializeField] bool _enemyDefultStartPos = true;
+    [Header("__________Settings__________")]
+    [Min(0f)][SerializeField] float _speed = 1.0f;
     [SerializeField] bool _enemyIdle = true;
+    [Min(0f)][SerializeField] float _idleTime = 1f;
+    [Min(0.01f)][SerializeField] float minDystans = 0.1f;
+    [SerializeField] bool _enemyDefultStartPos = true;
 
+    [Header("__________Edit__________")]
+    [SerializeField] bool ADD_POS = false;
+    [SerializeField] bool CLEAR_POS = false;
+
+    [Header("__________Debug__________")]
     [SerializeField] bool _showDebug = false;
-    [SerializeField] float _debugSize = .5f;
-    [SerializeField] Color _debugColor = Color.red;
+    [Range(0.1f, 2f)][SerializeField] float _debugSize = .5f;
+    [SerializeField] Color _debugColor = Color.white;
+    [SerializeField] Color _debugFollowPathColor = Color.green;
     int _followPointIndex;
 
+    [Header("__________Path__________")]
     [SerializeField] List<Vector3> _path = new List<Vector3>();
 
+
     float _timer;
-    bool _waiting = false;
+    bool _ideling = false;
 
     void _MoveToPoint()
     {
         _Idle();
-        if (_waiting)
+        if (_ideling)
             return;
 
         // Calculate the direction from the current position to the target position
@@ -38,7 +48,7 @@ public class Enemy : MonoBehaviour
         if (minDystans >= (_path[_followPointIndex] - transform.position).magnitude)
         {
             _NextPoint();
-            _waiting = true;
+            _ideling = true;
         }
     }
     void _NextPoint()
@@ -51,13 +61,13 @@ public class Enemy : MonoBehaviour
     }
     void _Idle()
     {
-        if (_enemyIdle && _waiting)
+        if (_enemyIdle && _ideling)
         {
             _timer += Time.deltaTime;
-            if (_timer >= _waitTime)
+            if (_timer >= _idleTime)
             {
                 _timer = 0;
-                _waiting = false;
+                _ideling = false;
             }
         }
     }
@@ -71,25 +81,48 @@ public class Enemy : MonoBehaviour
         for (int i = 0; i < _path.Count; i++)
         {
             if (_followPointIndex == i)
-                Gizmos.color = Color.green;
+                Gizmos.color = _debugFollowPathColor;
 
-            Gizmos.DrawLine(_path[i] + Vector3.forward * _debugSize, _path[i] + -Vector3.forward * _debugSize);
-            Gizmos.DrawLine(_path[i] + Vector3.right * _debugSize, _path[i] + -Vector3.right * _debugSize);
+            Gizmos.DrawSphere(_path[i], _debugSize);
 
-            Gizmos.color = _debugColor;
+            if (_followPointIndex == i)
+                Gizmos.color = _debugColor;
         }
         for (int i = 0; i < _path.Count - 1; i++)
         {
+            if (_followPointIndex == i)
+                Gizmos.color = _debugFollowPathColor;
+
             Gizmos.DrawLine(_path[i], _path[i + 1]);
+
+            if (_followPointIndex == i)
+                Gizmos.color = _debugColor;
         }
         Gizmos.DrawLine(_path[_path.Count - 1], _path[0]);
     }
+    private void OnValidate()
+    {
+        if (ADD_POS)
+        {
+            ADD_POS = false;
+            _path.Add(transform.position);
+        }
+        if (CLEAR_POS)
+        {
+            CLEAR_POS = false;
+            _path.Clear();
+            if (_enemyDefultStartPos)
+            {
+                _path.Add(transform.position);
+            }
+        }
 
+    }
     void Start()
     {
         if (_enemyDefultStartPos)
         {
-            _path.Insert(0, transform.position);
+            transform.position = _path[0];
         }
     }
 
