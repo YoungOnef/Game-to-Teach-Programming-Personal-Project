a16f1f9e6e9c501774a9bb98ab6e637dd91f8928
YoungOne <72264732+YoungOnef@users.noreply.github.com>
2023-01-16 14:04:38 +0000

    Fully Functional
    Fully Functional

--- Assets/scripts/ScriptManager.cs
+++ Assets/scripts/ScriptManager.cs
@@ -1,10 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using MoonSharp.Interpreter;
 using TMPro;
 using System;
-using UnityEngine.Events;
 
 
 public class ScriptManager : MonoBehaviour
@@ -13,26 +11,42 @@ public class ScriptManager : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
-    [Min(0f)][SerializeField] float speed = 1f;
 
     [SerializeField] bool __TimerON = false;
     [SerializeField] float __TimerTime = -1f;
     [SerializeField]
-    Vector3 CubeTargetPos = new Vector3(0, 0, 0);
     bool Moving = false;
     float MovingTimer = -1;
     string code;
-    private Vector3 Move(Vector3 vector3, int Distance)
+    Vector3 directionMovement = new Vector3(0, 0, 0);
+    Vector3 directionTurn = new Vector3Int(0, 0, 0);
+    Vector3 currnetTurnAngle = new Vector3Int(0, 0, 0);
+
+
+    private void Move(Vector3 Direction, int Distance)
     {
         Wait(Distance);
-        return new Vector3(0, 0, 0);
+        MovingTimer = Distance;
+        directionMovement = Direction;
+        Moving = true;
     }
-    
+    private void Turn(int Angle)
+    {
+        Wait(1);
+        MovingTimer = 1;
+        directionTurn = new Vector3Int(0, Angle, 0);
+        currnetTurnAngle += new Vector3Int(0, Angle, 0);
+        Moving = true;
+    }
 
-    public void MoveForward(int Distance = 1) => CubeTargetPos += Move(Vector3.forward,Distance);
-    public void MoveRight(int Distance = 1) => CubeTargetPos += Move(Vector3.right, Distance);
-    public void MoveBack(int Distance = 1) => CubeTargetPos += Move(Vector3.back, Distance);
-    public void MoveLeft(int Distance = 1) => CubeTargetPos += Move(Vector3.left, Distance);
+    public void TurnRight() => Turn(90);
+    public void TurnLeft() => Turn(-90);
+
+
+    public void MoveForward(int Distance = 1) => Move(player.transform.forward,Distance);
+    public void MoveRight(int Distance = 1) => Move(player.transform.right, Distance);
+    public void MoveBack(int Distance = 1) => Move(-player.transform.forward, Distance);
+    public void MoveLeft(int Distance = 1) => Move(-player.transform.right, Distance);
     public void Wait(float seconds = 1)
     {
         __TimerON = true;
@@ -43,17 +57,27 @@ public class ScriptManager : MonoBehaviour
         uIManager = GameObject.Find("UIManager").GetComponent<UIManager>();
     }
 
-
     private IEnumerator _Start()
     {
         var _readyCode = "function Start()\n" + code + "\nend";
         // Load the code and get the returned function
         Script script = new Script(CoreModules.None);
+
+        
         script.Globals["MoveForward"] = (Action<int>)MoveForward;
         script.Globals["MoveRight"] = (Action<int>)MoveRight;
         script.Globals["MoveBack"] = (Action<int>)MoveBack;
         script.Globals["MoveLeft"] = (Action<int>)MoveLeft;
+
+        script.Globals["TurnRight"] = (Action)TurnRight;
+        script.Globals["TurnLeft"] = (Action)TurnLeft;
+
         script.Globals["Wait"] = (Action<float>)Wait;
+
+        script.Globals["WhatsInFront"] = (Func<string, string, float, bool>)WhatsInFront;
+
+        script.Globals["print"] = (Action<DynValue>)uIManager.PrintToDebugLogAndTextArea;
+
         //script.LoadFunction("function Wait (n) return ('Wait' ,n) end \n");
         script.DoString(_readyCode);
 
@@ -74,7 +98,6 @@ public class ScriptManager : MonoBehaviour
             if (result.Type != DataType.YieldRequest)
                 break;
 
-
             if (__TimerON)
             {
                 yield return new WaitForSeconds(__TimerTime);
@@ -85,14 +108,135 @@ public class ScriptManager : MonoBehaviour
             result = coroutine.Coroutine.Resume();
         }
     }
-
+    private void Update()
+    {
+        if (Moving)
+        {
+            MovingTimer -= Time.deltaTime;
+            player.transform.position += directionMovement * Time.deltaTime;
+            player.transform.Rotate(directionTurn * Time.deltaTime);
+            if (MovingTimer <= 0)
+            {
+                player.transform.rotation = Quaternion.Euler(currnetTurnAngle);
+                player.transform.position = new Vector3(Mathf.Round(player.transform.position.x), player.transform.position.y, Mathf.Round(player.transform.position.z));
+                directionMovement = Vector3.zero;
+                directionTurn = Vector3.zero;
+                Moving = false;
+                MovingTimer = -1;
+            }
+        }
+    }
 
     public void InputText()
     {
         code = uIManager.inputField.GetComponent<TMP_InputField>().text;
-        
+       
         StartCoroutine(_Start());
 
 
+    }
+
+
+    // Set up a function that takes a string as an argument
+    // The string represents the direction that we want to check
+    bool WhatsInFront(string direction, string tag, float maxDistance = 1f)
+    {
+        // Set the starting position of the ray
+        Vector3 startingPosition = player.transform.position;
+
+
+        // Set up a variable to store the result of the raycast
+        RaycastHit hit;
+
+        // Check for objects in the specified direction
+        if (direction == "front")
+        {
+            // Check for objects in front of the Player
+            Vector3 forwardDirection = player.transform.forward;
+            if (Physics.Raycast(startingPosition, forwardDirection, out hit, maxDistance))
+            {
+                if (hit.transform.tag == tag)
+                {
+                    // An object with the specified tag was found in the specified direction
+                    return true;
+                }
+                else
+                {
+                    // An object was found in the specified direction, but it doesn't have the specified tag
+                    return false;
+                }
+            }
+            else
+            {
+                // No object was found in the specified direction
+                return false;
+            }
+        }
+        else if (direction == "back")
+        {
+            // Check for objects behind the Player
+            Vector3 backDirection = -player.transform.forward;
+            if (Physics.Raycast(startingPosition, backDirection, out hit, maxDistance))
+            {
+                if (hit.transform.tag == tag)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (direction == "left")
+        {
+            // Check for objects to the left of the Player
+            Vector3 leftDirection = -player.transform.right;
+            if (Physics.Raycast(startingPosition, leftDirection, out hit, maxDistance))
+            {
+                if (hit.transform.tag == tag)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (direction == "right")
+        {
+            // Check for objects to the right of the Player
+            Vector3 rightDirection = player.transform.right;
+            if (Physics.Raycast(startingPosition, rightDirection, out hit, maxDistance))
+            {
+                if (hit.transform.tag == tag)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            // The specified direction is not valid
+            Debug.LogError("Invalid direction: " + direction);
+            return false;
+        }
     }
 }
